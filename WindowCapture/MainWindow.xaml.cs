1d3c6a2eeee8513f8e7050aeb6b21295bd7bd202
aborn <guobao.jiang@aliyun.com>
2024-06-01 11:18:37 +0800

    蓝牙相关代码

--- WindowCapture/MainWindow.xaml.cs
+++ WindowCapture/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Text;
+using System.Windows;
 using System.Windows.Interop;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
 using WindowCapture.ScreenSelect;
 
 namespace WindowCapture
@@ -44,13 +48,102 @@ namespace WindowCapture
             return IntPtr.Zero;
         }
 
+        /// <summary>
+        /// 发送蓝牙内容 https://github.com/inthehand/32feet/blob/main/Samples/BluetoothClassicConsole/Program.cs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendFileCallback(object sender, RoutedEventArgs e)
+        {
+            BluetoothClient client = new BluetoothClient();
+            IReadOnlyCollection<BluetoothDeviceInfo> devices = client.DiscoverDevices();
+
+            BluetoothDeviceInfo device = null;
+            foreach (BluetoothDeviceInfo dev in devices) //设备搜寻           
+            {
+                if ("Nax".Equals(dev.DeviceName))
+                {
+                    device = dev;
+                    break;
+                }
+            }
+            if (device == null) { return; }
+
+            if (!device.Authenticated)
+            {
+                BluetoothSecurity.PairRequest(device.DeviceAddress, "1234");
+            }
+
+            device.Refresh();
+            Console.WriteLine("Connecting..." + device.Authenticated);
+            try
+            {
+                if (!device.Connected)
+                {
+                    client.Connect(device.DeviceAddress, BluetoothService.SerialPort);
+                    Console.WriteLine("Connected!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Has Been Connected!");
+                }
+
+                var stream = client.GetStream();
+
+                if (stream != null)
+                {
+                    byte[] buffer = File.ReadAllBytes(@"D:\1.jpg");
+                    //stream.Write(buffer, 0, buffer.Length);
+
+                    StreamWriter sw = new StreamWriter(stream, System.Text.Encoding.ASCII);
+                    sw.Write(buffer);
+                    //sw.WriteLine("Hello world!\r\n\r\n");
+                    sw.Close();
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// https://tisyang.github.io/post/2016-05-27-bluetooth-serialport-with-32feet-net-library/
+        /// https://blog.csdn.net/IFuWantMe/article/details/110658115
+        /// </summary>
+        /// <param name="bluetoothClient"></param>
+        private void ReceiveData(BluetoothClient bluetoothClient)
+        {
+            bool isConnected = bluetoothClient.Connected;
+            while (isConnected)
+            {
+                try
+                {
+                    string receive = string.Empty;
+                    Stream peerStream = bluetoothClient.GetStream();
+                    byte[] buffer = new byte[255];
+                    peerStream.Read(buffer, 0, 255);
+                    receive = Encoding.UTF8.GetString(buffer).ToString().Replace("\0", "");
+                    //Console.ReadKey();
+                    Console.Write(receive);
+                }
+                catch (Exception e)
+                {
+                    Console.Write("Error:" + e);
+                    break;
+                }
+            }
+        }
+
         private void ScreenRecordRectCallback(object sender, RoutedEventArgs e)
         {
             ScreenRectSelect();
         }
 
         private void ScreenRectSelect()
-        {   
+        {
             // this.Hide();
             App.Current.MainWindow.WindowState = WindowState.Minimized;
             Thread.Sleep(150);  // 待当前窗口最小化
